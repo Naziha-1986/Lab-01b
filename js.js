450e0413cb7472372955a8c0ee58818679b48106
Naziha-1986 <nazihaqawasmeh@gmail.com>
2021-06-20 21:21:53 +0300

    made some changes

--- js.js
+++ js.js
@@ -1,33 +1,31 @@
 let userName=prompt('what is your name?');
-console.log(userName);
+console.log("what is your name?"+userName);
+alert("welcome "+userName)
 
 
 let userAge=prompt('how old are you?')
-
 if(userAge<25){
     alert('hello')
 }else{
-    alert('welcome')
+    alert('nice to meet you')
 }
-console.log(userAge);
+console.log("how old are you?"+userAge);
 
 
 
 let favMovie=prompt('what is your favorite movie?');
-if(favMovie='godtahter'){
-    alert('meeee tooooo')
-}else{
-    alert('it is a good one')
-}
-
-console.log(favMovie);
+ alert('it is a greatt one')
+console.log("what is your favorite movie?"+favMovie);
 
 
-let favFood=prompt('what is your favorite food?')
-if(favFood='Mansaf'){
-    alert('yum-yum')
-}else{
-        alert("so delicious")
-}
+let favFood=prompt('what is your favorite food?');
+alert('yum-yum')
+console.log("what is your favorite food?"+favFood);
 
-console.log(favFood);
+
+let confirm= confirm('can you tell me if  these information are true ?');
+your name is: ${userName};
+your age is: ${userAge};
+your favorite movie is: ${favMovie};
+your favorite food is: ${favFood};
+if it is false click cancel
