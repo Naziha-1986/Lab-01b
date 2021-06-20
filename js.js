8cf17030ba2bd0126c59ea20b7c9fbf570001e8b
Naziha-1986 <nazihaqawasmeh@gmail.com>
2021-06-20 17:03:30 +0300

    added html,css,js files

---
+++ js.js
@@ -0,0 +1,33 @@
+let userName=prompt('what is your name?');
+console.log(userName);
+
+
+let userAge=prompt('how old are you?')
+
+if(userAge<25){
+    alert('hello')
+}else{
+    alert('welcome')
+}
+console.log(userAge);
+
+
+
+let favMovie=prompt('what is your favorite movie?');
+if(favMovie='godtahter'){
+    alert('meeee tooooo')
+}else{
+    alert('it is a good one')
+}
+
+console.log(favMovie);
+
+
+let favFood=prompt('what is your favorite food?')
+if(favFood='Mansaf'){
+    alert('yum-yum')
+}else{
+        alert("so delicious")
+}
+
+console.log(favFood);
